8e87a935bbe8ee55bc0586949ba1e69a9616e70c
Marlon B v/d Linde <marlon@kbye.co.za>
2017-04-17 21:25:28 +0200

    Remove unused requestId and comments.

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,6 @@ use App\Library\ApiVersionTool;
 
 $apiVersion = 'v' . ApiVersionTool::validateAndGetApiVersionFromHeader();
 
-$requestId = uniqid('req_', true);
-
 $app->get('/', function () use ($app) {
     return $app->version();
 });
@@ -25,7 +23,7 @@ $app->get('/', function () use ($app) {
  * User
  */
 //$app->put('/user/{uniq}', $apiVersion.'\UserController@update');  // @todo
-$app->get('/user/{email}', ['middleware' => 'auth', 'uses' => $apiVersion.'\UserController@get', 'gom' => $requestId]);
+$app->get('/user/{email}', ['middleware' => 'auth', 'uses' => $apiVersion.'\UserController@get']);
 $app->post('/user', $apiVersion.'\UserController@create');
 
 
@@ -41,12 +39,3 @@ $app->get('/package/{id}', ['middleware' => 'auth', 'uses' => $apiVersion.'\Pack
  */
 $app->post('/token', $apiVersion.'\TokenController@create');
 $app->get('/token/{otp}', $apiVersion.'\TokenController@verify');
-
-
-// @todo temp notes for token endpoints.
-
-// /token - request a new token, supplying a uniq. This creates a token, in a disabled state, and sends an email or
-//          some message to the uniq containing a once-off-code with a limit ttl. respond with 201 created or 404
-
-// /verify - once a uniq received his once-off-code, this endpoint will verify it, and enable the token.
-//            it will be neccesary to now use the newly generated token and otp to validate and enable.
