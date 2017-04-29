50b2e1d16913d0485d66b639d029fe43eb6604b7
Marlon B v/d Linde <marlon@kbye.co.za>
2017-04-29 16:51:59 +0200

    GPLv3 entire project, and adding specific headers where it makes sense.
    The entire package is licensed from here onwards, under GPLv3. Boilerplate per file is a reminder only, and does not make sense adding it everywhere liberally, both for space reasons, and that the opensource community are far from being idiots. Also, where the code was generated by lumen, and non-specific to the project, no boilerplate was added. Thank you gents and ladies of the code world.

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -1,5 +1,23 @@
 <?php
 
+/**
+ * Arid-Grazer Engine - A Multi-User messaging system, with a Post Office like smell.
+ * Copyright (C) 2017  Marlon B van der Linde
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 namespace App\Http\Middleware;
 
 use Closure;
